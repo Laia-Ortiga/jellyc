7cd3a3bb87534579c3408dd1248feedf28958d69
Laia Ortiga <laia.sonder@gmail.com>
2025-09-06 20:33:17 +0200

    Add exit to test/opengl scripts

--- test/opengl/run-c.sh
+++ test/opengl/run-c.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 ../../build/jellyc *.jel ../../lib/libc.jel
+[ $? -eq 0 ] || exit 1
 clang a.c glad/src/gl.c -Iglad/include -lm -lglfw && ./a.out
--- test/opengl/run-llvm.sh
+++ test/opengl/run-llvm.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 ../../build/jellyc -backend=llvm *.jel ../../lib/libc.jel
+[ $? -eq 0 ] || exit 1
 clang a.ll glad/src/gl.c -Iglad/include -lm -lglfw && ./a.out
