be772d1b95ed5186779bff4062c90e2ad72eb4bd
Laia Ortiga <laia.sonder@gmail.com>
2025-09-15 19:28:16 +0200

    Separate generic terms in tir.h

--- src/data/tir.h
+++ src/data/tir.h
@@ -257,7 +257,6 @@ TirId get_any_struct_type_field(TirContext c, TirId type, int32_t index);
 EnumType get_enum_type(TirContext c, TirId type);
 TaggedType get_tagged_type(TirContext c, TirId type);
 TirId get_tagged_type_arg(TirContext c, TirId type, int32_t index);
-GenericTerm get_generic_term(TirContext c, TirId term);
 
 TirId remove_any_pointer(TirContext c, TirId a);
 TirId remove_pointer(TirContext c, TirId a);
@@ -272,14 +271,12 @@ bool type_is_linear(TirContext c, TirId a);
 bool type_is_unknown_size(TirContext c, TirId a);
 bool is_equality_type(TirContext c, TirId a);
 bool is_relative_type(TirContext c, TirId a);
+
 bool int_fits_in_type(int64_t i, TirId a, Target target);
 TirId bigger_primitive_type(TirId a, TirId b, Target target);
-
 int32_t sizeof_pointer(Target target);
 int32_t alignof_type(TirContext c, TirId type, Target target);
 int64_t sizeof_type(TirContext c, TirId type, Target target);
-void print_type(FILE *file, TirContext c, TirId type);
-void debug_type(TirContext c, TirId type);
 
 static inline bool type_is_fixed_int(TirId type) {
     return type.id >= TYPE_i8 && type.id <= TYPE_char;
@@ -297,7 +294,10 @@ static inline bool type_is_arithmetic(TirId type) {
     return type_is_int(type) || type_is_float(type);
 }
 
-// Type matching
+void print_type(FILE *file, TirContext c, TirId type);
+void debug_type(TirContext c, TirId type);
+
+// Type Matching
 
 typedef struct TypeMatcher {
     enum {
@@ -346,15 +346,19 @@ TirId new_variable(TirContext c, AstId node, TirId type, int32_t index, bool mut
 TirId new_unary_tir(TirContext c, TirTag tag, AstId node, TirId type, TirId a);
 TirId new_binary_tir(TirContext c, TirTag tag, AstId node, TirId type, TirId a, TirId b);
 TirId new_instr(TirContext c, TirTag tag, AstId node, TirId type, int32_t a, int32_t b);
-TirId new_generic(
-    TirContext c,
-    TirId inner,
-    int32_t type_count,
-    TirId *types
-);
 
 TirId get_value_type(TirContext c, TirId value);
 ValueCategory get_value_category(TirContext c, TirId value);
 char const *get_value_str(TirContext c, TirId value);
 int64_t get_value_int(TirContext c, TirId value);
 double get_value_float(TirContext c, TirId value);
+
+// Other
+
+TirId new_generic(
+    TirContext c,
+    TirId inner,
+    int32_t type_count,
+    TirId *types
+);
+GenericTerm get_generic_term(TirContext c, TirId term);
