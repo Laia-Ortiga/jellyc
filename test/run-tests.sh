8a30f46d86fff839a01764798677b3908a297358
Laia Ortiga <laia.sonder@gmail.com>
2025-09-17 06:24:13 +0200

    Add new tests in run-tests.sh

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-build/jellyc -print-debug test/print_type.jel
-[ $? -eq 0 ] || echo "tests failed"
+
+test() {
+    eval "$@" &> /dev/null
+    [ $? -eq 0 ] && echo -e "\033[1;32m[success]\033[0m $@" || echo -e "\033[1;31m[failed]\033[0m $@"
+}
+
+export PATH=build:$PATH
+
+test jellyc test/hello_world.jel
+test jellyc lib/*.jel test/array.jel
+test jellyc lib/*.jel test/basic_lexer.jel
+test jellyc lib/*.jel test/fibonacci.jel
+test jellyc lib/*.jel test/opengl/*.jel
+test jellyc -print-debug test/print_type.jel
