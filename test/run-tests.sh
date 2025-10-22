2c2f0f23f480e9dec9bf5b266bc103e897103aad
Laia Ortiga <laia.sonder@gmail.com>
2025-10-22 18:41:13 +0200

    Check final compilation in run-tests.sh

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,24 +1,48 @@
 #!/usr/bin/env bash
 
+success_counter=0
+failure_counter=0
+
+success() {
+    echo -e "\033[1;32m[success]\033[0m $@"
+    success_counter=$((success_counter + 1))
+}
+
+failure() {
+    echo -e "\033[1;31m[failed]\033[0m $@"
+    failure_counter=$((failure_counter + 1))
+}
+
 test() {
-    eval "$@" &> /dev/null
-    [ $? -eq 0 ] && echo -e "\033[1;32m[success]\033[0m $@" || echo -e "\033[1;31m[failed]\033[0m $@"
+    eval "($@) &> /dev/null"
+    [ $? -eq 0 ] && success "$@" || failure "$@"
+}
+
+test_c() {
+    test "jellyc -backend=c $@ && gcc a.c -lm"
+}
+
+test_llvm() {
+    test "jellyc -backend=llvm $@ && clang a.ll -lm"
+}
+
+test_all() {
+    test_c "$@"
+    test_llvm "$@"
 }
 
 export PATH=build:$PATH
 
-test jellyc -backend=c    test/hello_world.jel
-test jellyc -backend=llvm test/hello_world.jel
-test jellyc -backend=c    lib/*.jel test/array.jel
-test jellyc -backend=llvm lib/*.jel test/array.jel
-test jellyc -backend=c    lib/*.jel test/basic_lexer.jel
-test jellyc -backend=llvm lib/*.jel test/basic_lexer.jel
-test jellyc -backend=c    lib/*.jel test/fibonacci.jel
-test jellyc -backend=llvm lib/*.jel test/fibonacci.jel
+test_all test/hello_world.jel
+test_all lib/*.jel test/array.jel
+test_all lib/*.jel test/basic_lexer.jel
+test_all lib/*.jel test/fibonacci.jel
 test jellyc -backend=c    lib/*.jel test/opengl/*.jel
 test jellyc -backend=llvm lib/*.jel test/opengl/*.jel
 test jellyc -print-debug test/print_type.jel
-test jellyc lib/*.jel test/feature/if.jel
-test jellyc lib/*.jel test/feature/switch.jel
-test jellyc lib/*.jel test/feature/while.jel
-rm -f a.c a.ll
+test_all lib/*.jel test/feature/if.jel
+test_all lib/*.jel test/feature/switch.jel
+test_all lib/*.jel test/feature/while.jel
+rm -f a.c a.ll a.out
+echo "$success_counter tests passed"
+echo "$failure_counter tests failed"
